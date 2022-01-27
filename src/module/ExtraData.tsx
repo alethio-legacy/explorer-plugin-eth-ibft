935e4f387983445559ab90ef5311da2a2a9fe1da
a.andreev <artem.v.andreev@gmail.com>
2022-01-27 11:19:04 +0200

    refactored the extra data component

--- src/module/ExtraData.tsx
+++ src/module/ExtraData.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import { LayoutRow } from "@alethio/ui/lib/layout/content/LayoutRow";
 import { LayoutRowItem } from "@alethio/ui/lib/layout/content/LayoutRowItem";
 import { Label } from "@alethio/ui/lib/data/Label";
-import { DecodedHexData } from "@alethio/ui/lib/data/hex/DecodedHexData";
-import { ValueBox } from "@alethio/ui/lib/layout/content/box/ValueBox";
-import { NumberBox } from "@alethio/ui/lib/data/box/NumberBox";
 import { HashValueBox } from "@alethio/ui/lib/data/box/HashValueBox";
 import { AddressHashBox } from "@alethio/explorer-ui/lib/box/account/AddressHashBox";
 import { IIbftDetails } from "../data/IIbftDetails";
@@ -18,50 +15,40 @@ export interface IExtraDataProps {
 
 export class ExtraData extends React.Component<IExtraDataProps> {
     render() {
-        let { ibftDetails, translation: tr, locale } = this.props;
+        let { ibftDetails, translation: tr } = this.props;
 
         return (
             <>
-                { ibftDetails.extraData.replace("0", "") === "x" ?
                 <LayoutRow>
                     <LayoutRowItem autoHeight>
-                        <Label>{tr.get("blockView.content.extraData.label")}</Label>
-                        <DecodedHexData data={ibftDetails.extraData} />
+                        <Label>{tr.get("blockView.content.ibft.extraData")}</Label>
+                        <HashValueBox>{ibftDetails.extraData}</HashValueBox>
                     </LayoutRowItem>
-                </LayoutRow> : null}
+                </LayoutRow>
                 <LayoutRow>
                     <LayoutRowItem fullRow>
-                        <Label>{tr.get("blockView.content.ibft2.validators")}</Label>
+                        <Label>{tr.get("blockView.content.ibft.validated")}</Label>
+                        <AddressHashBox>{ibftDetails.validated}</AddressHashBox>
+                    </LayoutRowItem>
+                </LayoutRow>
+                <LayoutRow>
+                    <LayoutRowItem fullRow>
+                        <Label>{tr.get("blockView.content.ibft.validators")}</Label>
                         { ibftDetails.validators.map((item) =>
                             <AddressHashBox key={item}>{item}</AddressHashBox>
                         ) }
                     </LayoutRowItem>
                 </LayoutRow>
-                { ibftDetails.votes.length !== 0 &&
-                    <LayoutRow>
-                        <LayoutRowItem fullRow>
-                            <Label>{tr.get("blockView.content.ibft2.voting")}</Label>
-                            { ibftDetails.votes.map((item) =>
-                                <React.Fragment key={item.address}>
-                                    <AddressHashBox>{item.address}</AddressHashBox>
-                                    <ValueBox>{item.vote ?
-                                        tr.get("blockView.content.ibft2.added") :
-                                        tr.get("blockView.content.ibft2.removed")}</ValueBox>
-                                </React.Fragment>
-                            )}
-                        </LayoutRowItem>
-                    </LayoutRow>
-                }
                 <LayoutRow>
-                    <LayoutRowItem>
-                        <Label>{tr.get("blockView.content.ibft2.round")}</Label>
-                        <NumberBox value={ibftDetails.blockTries} locale={locale}></NumberBox>
+                    <LayoutRowItem fullRow>
+                        <Label>{tr.get("blockView.content.ibft.seal")}</Label>
+                        <HashValueBox>{ibftDetails.seal}</HashValueBox>
                     </LayoutRowItem>
                 </LayoutRow>
                 { ibftDetails.commitSeals.length !== 0 &&
                     <LayoutRow>
                         <LayoutRowItem fullRow>
-                            <Label>{tr.get("blockView.content.ibft2.commitSeals")}</Label>
+                            <Label>{tr.get("blockView.content.ibft.commitSeals")}</Label>
                             { ibftDetails.commitSeals.map((item) =>
                                 <React.Fragment key={item}>
                                     <HashValueBox>{item}</HashValueBox>
